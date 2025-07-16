984121ff6f3034aba28a559d33542ff280dcb41d
Kartheek <ai07kartheek@gmail.com>
2025-07-16 23:48:38 +0530

    Added Airport resource crud

---
+++ src/controllers/airport-controller.js
@@ -0,0 +1,101 @@
+const { StatusCodes } = require("http-status-codes");
+
+const { AirportService } = require("../services");
+const { SuccessResponse, ErrorResponse } = require("../utils/common");
+
+/**
+ * POST : /aiports
+ * req-body { name: 'IGI', code: 'DEL', cityId: 1, address: 'New Delhi' }
+ */
+async function createAirport(req, res) {
+    try {
+        const airport = await AirportService.createAirport({
+            name: req.body.name,
+            code: req.body.code,
+            address: req.body.address,
+            cityId: req.body.cityId,
+        });
+        SuccessResponse.message = 'Successfully created an airport';
+        SuccessResponse.data = airport;
+        return res
+            .status(StatusCodes.CREATED)
+            .json(SuccessResponse);
+    }
+    catch (error) {
+        ErrorResponse.message = 'Something went wrong while creating an Airport';
+        ErrorResponse.error = error;
+        return res
+            .status(error.statusCode)
+            .json(ErrorResponse);
+    }
+}
+
+/**
+ * GET : /airports
+ * req-body { }
+ */
+async function getAirports(req, res) {
+    try {
+        const airports = await AirportService.getAirports();
+        SuccessResponse.data = airports;
+        return res
+            .status(StatusCodes.OK)
+            .json(SuccessResponse);
+    }
+    catch (error) {
+        ErrorResponse.message = 'Something went wrong while fetching Airports';
+        ErrorResponse.error = error;
+        return res
+            .status(error.statusCode)
+            .json(ErrorResponse);
+    }
+}
+
+/**
+ * GET : /airports/:id
+ * req-body { }
+ */
+async function getAirport(req, res) {
+    try {
+        const airport = await AirportService.getAirport(req.params.id);
+        SuccessResponse.data = airport;
+        return res
+            .status(StatusCodes.OK)
+            .json(SuccessResponse);
+    }
+    catch (error) {
+        ErrorResponse.message = 'Something went wrong while fetching an Airport';
+        ErrorResponse.error = error;
+        return res
+            .status(error.statusCode)
+            .json(ErrorResponse);
+    }
+}
+
+/**
+ * DELETE : /airports/:id
+ * req-body { }
+ */
+async function destroyAirport(req, res) {
+    try {
+        const response = await AirportService.destroyAirport(req.params.id);
+        SuccessResponse.data = response;
+        return res
+            .status(StatusCodes.OK)
+            .json(SuccessResponse);
+    }
+    catch (error) {
+        ErrorResponse.message = 'Something went wrong while deleting an Airport';
+        ErrorResponse.error = error;
+        return res
+            .status(error.statusCode)
+            .json(ErrorResponse);
+    }
+}
+
+module.exports = {
+    createAirport,
+    getAirports,
+    getAirport,
+    destroyAirport
+}
